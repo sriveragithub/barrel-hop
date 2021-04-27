c77da837a8aa1a7278a0ae374edc1504ee843ced
Skyler Rivera <skylerrivera17@yahoo.com>
2021-04-27 12:54:55 -0400

    restructured project to include barrel-hop components - working on passing API keys from process.env to static JS file

--- index.js
+++ index.js
@@ -1,10 +1,21 @@
 const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
+require('dotenv').config()
+const weatherKey =  process.env.WEATHER_SECRET_API
+const unsplashKey =  process.env.UNSPLASH_API
+
+console.log(process.env)
+console.log(weatherKey)
+console.log(unsplashKey)
+
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
+  .get('/', (req, res) => res.render('pages/index', {
+      weather: weatherKey,
+      unsplash: unsplashKey
+  }))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
